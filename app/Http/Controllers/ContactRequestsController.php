e7e2207ae777f5af7a41724b5018562a84a60c76
Pallav Dubey <pallav.dubey@gmail.com>
2022-04-07 07:19:48 -0400

    add api for get all and create

--- app/Http/Controllers/ContactRequestsController.php
+++ app/Http/Controllers/ContactRequestsController.php
@@ -7,15 +7,26 @@ use Illuminate\Http\Request;
 
 class ContactRequestsController extends Controller
 {
-    public function getAll()
-    {
-        return ContactRequest::collection(ContactRequest::all());
-    }
-
     public function list()
     {
         return view('contactRequests.list', [
             'contact_requests' => ContactRequest::all()
         ]);
+    }
+
+    public function getAll()
+    {
+        return ContactRequest::all();
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required',
+            'message' => 'required',
+        ]);
+
+        return ContactRequest::create($request->all());
     }
 }
