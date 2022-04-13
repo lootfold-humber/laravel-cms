8e9a4bb5a727f36735f00e0c4f955a1213b5c0f1
Pallav Dubey <pallav.dubey@gmail.com>
2022-04-12 20:31:00 -0400

    update database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\ContactRequest;
+use App\Models\Education;
+use App\Models\EducationLevel;
 use App\Models\User;
 use App\Models\Type;
 use App\Models\Project;
+use App\Models\Skill;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,12 +22,24 @@ class DatabaseSeeder extends Seeder
     {
 
         User::truncate();
-        Type::truncate();
-        Project::truncate();
-        
         User::factory()->count(2)->create();
+
+        Type::truncate();
         Type::factory()->count(3)->create();
+
+        Project::truncate();
         Project::factory()->count(4)->create();
-            
+
+        ContactRequest::truncate();
+        ContactRequest::factory()->count(5)->create();
+
+        EducationLevel::truncate();
+        EducationLevel::factory()->count(5)->create();
+
+        Education::truncate();
+        Education::factory()->count(3)->create();
+
+        Skill::truncate();
+        Skill::factory()->count(5)->create();
     }
 }
